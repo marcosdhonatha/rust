166e00ab7fb1f180378256411a8c618649830ebc
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2025-06-04 14:28:10 -0400

    organizing folders

---
+++ projects/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-802907664bb2463b.d
@@ -0,0 +1,5 @@
+/home/marcos/Documentos/rust/projects/hello_cargo/target/debug/deps/libhello_cargo-802907664bb2463b.rmeta: src/main.rs
+
+/home/marcos/Documentos/rust/projects/hello_cargo/target/debug/deps/hello_cargo-802907664bb2463b.d: src/main.rs
+
+src/main.rs:
