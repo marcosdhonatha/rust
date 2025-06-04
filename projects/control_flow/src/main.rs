7f6858a3ac23f8a7dbcdb9df360135b7d9a6bd0f
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2025-06-04 09:49:02 -0400

    format document

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -16,71 +16,67 @@ fn main() {
     test4();
 }
 
-fn test()->i32{
-    let cond=true;
-    let number= if cond {
-        18
-    }else {
-        17
-    };
-number
+fn test() -> i32 {
+    let cond = true;
+    let number = if cond { 18 } else { 17 };
+    number
 }
 
-fn test2(){
-    let mut counter=0;
+fn test2() {
+    let mut counter = 0;
     let result = loop {
-        counter+=1;
-        if counter == 10{
-            break counter*10;
+        counter += 1;
+        if counter == 10 {
+            break counter * 10;
         }
     };
 
     println!("The result is {result}");
 }
 
-fn test3(){
-    let mut count =0;
-    'count_loop: loop{
+fn test3() {
+    let mut count = 0;
+    'count_loop: loop {
         println!("Count:{}", count);
-        let mut remaining =10;
-        
-        loop{
+        let mut remaining = 10;
+
+        loop {
             println!("Remaining: {}", remaining);
-            if remaining ==0 {
+            if remaining == 0 {
                 break;
             }
-            if count==2{
+            if count == 2 {
                 break 'count_loop;
             }
-            remaining-=1;
+            remaining -= 1;
         }
-        count+=1;
+        count += 1;
     }
     println!("End count = {count}");
     println!("######################");
 }
 
-fn test4(){
-    let mut number =3;
-    while number != 0{
+fn test4() {
+    let mut number = 3;
+    while number != 0 {
         println!("{number}");
-        number -=1;
+        number -= 1;
     }
     println!("######################");
 
     let a = [10, 20, 30, 40, 50];
-    let mut index=0;
+    let mut index = 0;
 
-    while index<a.len(){
-        println!("{}",a[index]);
-        index+=1;
+    while index < a.len() {
+        println!("{}", a[index]);
+        index += 1;
     }
     println!("######################");
-for element in a {
-    println!("{element}")
-}
-println!("######################");
-for number in (1..4).rev() {
-    println!("{number}")
-}
+    for element in a {
+        println!("{element}")
+    }
+    println!("######################");
+    for number in (1..4).rev() {
+        println!("{number}")
+    }
 }
