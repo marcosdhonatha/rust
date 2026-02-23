a03e49539efd646745fbf0ca1c77a74182e72d7a
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2026-02-23 19:26:39 -0300

    refactoring code

---
+++ Capítulo 12 - An I-O Project - Building a Command Line Program/minigrep/src/lib.rs
@@ -0,0 +1,25 @@
+use std::{ fs::read_to_string, error::Error};
+pub struct Config {
+   pub query: String,
+   pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config)-> Result<(), Box<dyn Error>> {
+    let contents =
+        read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+    Ok(())
+}
