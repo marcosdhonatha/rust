188e04942b008516abcbd0e916b383d43440f7c1
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2026-02-25 20:24:17 -0300

    add code example

---
+++ Capitulo 13 - Functional Language Features - Iterators and Closures/closures/src/main.rs
@@ -0,0 +1,49 @@
+use std::option;
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue, ShirtColor::Red, ShirtColor::Red, ShirtColor::Red, ShirtColor::Red],
+    };
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Blue,
+    Red,
+}
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
