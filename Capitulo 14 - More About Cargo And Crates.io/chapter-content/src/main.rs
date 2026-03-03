be161dcc2151bf75aeccd29d45ca80ee20232716
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2026-03-03 11:03:31 -0300

    add chapter

---
+++ Capitulo 14 - More About Cargo And Crates.io/chapter-content/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+     
+}
+
+//Customizing Builds with Release Profiles:
+
+// Cargo has two main profiles: the dev profile Cargo uses when you run
+// cargo build, and the release profile Cargo uses when you run cargo build
+// --release. The dev profile is defined with good defaults for development,
+// and the release profile has good defaults for release builds.
+
+//Publishing a Crate to Crates.io:
+    //Making Useful Documentation Comments
