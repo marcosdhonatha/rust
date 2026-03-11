f736de9d92b398ec1c8928ff5d7e0048313ed722
marcosdhonatha <marcosdhonathacachoeirafilho@gmail.com>
2026-03-11 20:52:56 -0300

    add first example

--- Capítulo 16 - Fearless Concurrency/chapter-content/src/main.rs
+++ Capítulo 16 - Fearless Concurrency/chapter-content/src/main.rs
@@ -1,3 +1,16 @@
+//Creating a New Thread with spawn:
+
+use std::{thread, time::Duration};
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(||{
+        for i in 1..10{
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+for i in 1..5{
+    println!("hi number {i} from the main thread!");
+    thread::sleep(Duration::from_millis(1));
+}
 }
